79fcb1cdd1ab2b8b9c7f5166bbdb121e7fbe3192
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2014-06-05 23:47:40 -0300

    agregado calendario de google maps, deshabilitada la session nosotros

--- application/views/disponibilidad.php
+++ application/views/disponibilidad.php
@@ -6,7 +6,8 @@
 <br />
 
 <p>
-    Calendario de fechas disponibles para alquiler de la cabaña. 
+    Calendario de fechas disponibles para consultar disponibilidad de la cabaña
+    a la hora de realizar reservas. 
 </p>
 <br />
 <br />
@@ -14,9 +15,5 @@
     <iframe src="https://www.google.com/calendar/embed?src=l42c2slo3u4at5f7p1m4ipe1so%40group.calendar.google.com&ctz=America/Buenos_Aires"
     style="border: 0" width="800" height="600" frameborder="0" scrolling="no">
     </iframe>
-
-<br />
-<br />
-<iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=l42c2slo3u4at5f7p1m4ipe1so%40group.calendar.google.com&amp;color=%232F6309&amp;ctz=America%2FNew_York" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>
 </div>
 
